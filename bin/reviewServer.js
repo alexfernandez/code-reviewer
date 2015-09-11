3c3c4381d1d0dff38184d2543cff92f5c6be45be
Alex Fernández <alex.fernandez@mediasmart.es>
2015-09-11 23:37:19 +0200

    Use credentials as default values, complain if not found.

--- bin/reviewServer.js
+++ bin/reviewServer.js
@@ -8,17 +8,30 @@
 
 // requires
 var stdio = require('stdio');
+var Log = require('log');
 var reviewServer = require('../lib/reviewServer');
 
+// globals
+var log = new Log('info');
+
 // init
+try
+{
+	var credentials = require('./credentials.json');
+}
+catch(exception)
+{
+	log.info('Please enter default values in a file called credentials.json');
+}
 var options = stdio.getopt({
-	token: {key: 't', args: 1, mandatory: true, description: 'Consumer token for Trello'},
-	key: {key: 'k', args: 1, mandatory: true, description: 'Key for Trello'},
-	secret: {key: 's', args: 1, mandatory: true, description: 'Secret value to access the server'},
+	token: {key: 't', args: 1, description: 'Consumer token for Trello'},
+	key: {key: 'k', args: 1, description: 'Key for Trello'},
+	secret: {key: 's', args: 1, description: 'Secret value to access the server'},
 	port: {key: 'p', args: 1, description: 'Port to start the server', default: 6003},
 	quiet: {key: 'q', description: 'Do not log any messages'},
 	debug: {key: 'd', description: 'Log debug messages'},
 });
+credentials.overwriteWith(options);
 
-reviewServer.start(options);
+reviewServer.start(credentials);
 
